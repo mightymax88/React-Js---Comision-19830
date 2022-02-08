b3eba5c461a995873301065e72121c920f6cbdbc
Maximiliano Filipuzzi <maxi.filipuzzi@gmail.com>
2022-02-08 17:25:11 -0300

    Se agrego useEffect y paso codigo a ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,66 +1,16 @@
 import './App.css';
-import React,{useState} from 'react';
+import React from 'react';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import ItemListContainer from './components/ItemListContainer.js';
 
-let is_ok = true;
-
-const data = [
-  {
-    id: 1,
-    title: "Pokemon Legends Arceus",
-    price: "12400",
-    thumbnail: "https://m.media-amazon.com/images/I/81vaoG0k9CS._AC_SY500_.jpg",
-    category: "Aventura, RPG",
-    description: "Nuevo videojuego de rol, precuela de Pokémon diamante y perla",
-    stock: "5"
-  },
-  {
-    id: 2,
-    title: "Mario Odyssey",
-    price: "9500",
-    thumbnail: "https://m.media-amazon.com/images/I/51NgDXzErYL._SY445_.jpg",
-    category: "Aventura",
-    description: "Videojuego de plataformas en tres dimensiones para Nintendo Switch.",
-    stock: "0"
-  },
-  {
-    id: 3,
-    title: "Mario Kart 8 Deluxe",
-    price: "10100",
-    thumbnail: "https://m.media-amazon.com/images/I/81iJG2js5-S._SY445_.jpg",
-    category: "Carreras",
-    description: "Undécima entrega de la serie Mario Kart, octava en consolas de Nintendo.",
-    stock: "2"
-  }
-]
-
 function App() {
-  const [games, setGames] = useState([]);
-
-  const customFetch = (timeout, data) => {
-    return new Promise ((resolve, reject) => {
-      setTimeout(() => {
-        if(is_ok){
-          resolve(data)
-        } else {
-          reject('KO')
-        }
-      }, timeout);
-    });
-  };
-
-  customFetch(2000,data)
-  .then (data => setGames(data))
-  .catch (error => alert ("Error en datos:", error))
-
   return (
     <div className="App">
       {/* Incorporación de navbar dentro dle componente NavBAR.js */}
       <NavBar />
       <h1>Sitio de compra juegos Nintendo Switch en Argentina</h1>
-      <ItemListContainer saludo="Bienvenido a nuestro sitio web!" games={games}/>
+      <ItemListContainer />
       <Footer />
     </div>
   );
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import ItemList from './ItemList';
 
-function ItemListContainer({saludo,games}){
+
+const data = [
+    {
+        id: 1,
+        title: "Pokemon Legends Arceus",
+        price: "12400",
+        thumbnail: "https://m.media-amazon.com/images/I/81vaoG0k9CS._AC_SY500_.jpg",
+        category: "Aventura, RPG",
+        description: "Nuevo videojuego de rol, precuela de Pokémon diamante y perla",
+        stock: "5"
+    },
+    {
+        id: 2,
+        title: "Mario Odyssey",
+        price: "9500",
+        thumbnail: "https://m.media-amazon.com/images/I/51NgDXzErYL._SY445_.jpg",
+        category: "Aventura",
+        description: "Videojuego de plataformas en tres dimensiones para Nintendo Switch.",
+        stock: "0"
+    },
+    {
+        id: 3,
+        title: "Mario Kart 8 Deluxe",
+        price: "10100",
+        thumbnail: "https://m.media-amazon.com/images/I/81iJG2js5-S._SY445_.jpg",
+        category: "Carreras",
+        description: "Undécima entrega de la serie Mario Kart, octava en consolas de Nintendo.",
+        stock: "2"
+    }
+]  
+
+let is_ok = true;
+
+function ItemListContainer(){
+    const saludo="Bienvenido a nuestro sitio web!";
+    const [games, setGames] = useState([]);
+
+    const customFetch = (data) => {
+        return new Promise ((resolve, reject) => {
+            if(is_ok){
+            resolve(data)
+            } else {
+            reject('KO')
+            }
+        });
+    };
+
+    //componentDidMount
+    useEffect(() => {
+        setTimeout(() => {
+            function getGames(){
+                customFetch(data)
+                    .then (data => setGames(data))
+                    .catch (error => alert ("Error en datos:", error))
+            }
+            getGames();
+        },2000);
+    },[]);
+
     return(
         <div>
             <div className='tituloILC'>
